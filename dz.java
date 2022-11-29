d7893442a89b683ee417036bf7a19b7d972197d5
De <spark49942@gmail.com>
2022-11-30 02:43:33 +0300

    DZ3

--- dz.java
+++ dz.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class dz {
     public static void main(String[] args) {
         Scanner work = new Scanner(System.in);
-        System.out.println("2. Введите желаемый размер массива: ");
+        System.out.print("2. Введите желаемый размер массива: ");
         String si = work.nextLine();
         int ze = Integer.parseInt(si);
         ArrayList<Integer> numbs = new ArrayList<>();
@@ -14,12 +14,23 @@ public class dz {
         for (int i = 0; i<ze; i++) {
             int randnum = ThreadLocalRandom.current().nextInt(0, 23);
             numbs.add(randnum);
-            if (randnum / 2 != 0) {
+            if (randnum % 2 != 0) {
                 odds.add(randnum);
             }
         }
         System.out.println(Arrays.asList(numbs) + " - изначальный вид массива.");
         System.out.println(Arrays.asList(odds) + " - массив нечётных чисел.");
         work.close();
+
+        int mini = numbs.get(0), maxi = 0, midi = 0;
+        for (int i = 0; i<ze; i++) {
+            int v1 = numbs.get(i);
+            if (v1 > maxi) { maxi = v1; }
+            else if (v1 < mini) { mini = v1;}
+            else {midi = v1;}
+        }
+        System.out.println("\n3.\n" + maxi + " - максимальное значение первого массива.");
+        System.out.println(mini + " - минимальное значение первого массива.");
+        System.out.println(midi + " - среднее значение первого массива.");
     }
 }
